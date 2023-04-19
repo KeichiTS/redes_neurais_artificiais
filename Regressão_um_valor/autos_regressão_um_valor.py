e2a1daef896274ae64958e9b160f46652f1de271
KeichiTS <kkeichi@gmail.co>
2023-04-19 15:44:46 -0300

    autos_regressão_um_valor_Ver2

--- Regressão_um_valor/autos_regressão_um_valor.py
+++ Regressão_um_valor/autos_regressão_um_valor.py
@@ -7,3 +7,16 @@ Created on Mon Apr 17 19:06:45 2023
 import pandas as pd
 
 base = pd.read_csv('autos.csv', encoding = 'ISO-8859-1')
+base = base.drop('dateCrawled', axis = 1)
+base = base.drop("dateCreated", axis = 1)
+base = base.drop('nrOfPictures', axis = 1)
+base = base.drop('postalCode', axis = 1)
+base = base.drop('lastSeen', axis = 1)
+
+base['name'].value_counts()
+base = base.drop('name', axis = 1)
+base['seller'].value_counts()
+base = base.drop('seller', axis = 1)
+base['offerType'].value_counts()
+base = base.drop('offerType', axis = 1)
+base['powerPS'].value_counts()
