de8b743a276613765d396141c6323f5c1750c64d
KeichiTS <kkeichi@gmail.co>
2023-04-19 20:51:20 -0300

    autos_regressão_um_valor_Ver4

--- Regressão_um_valor/autos_regressão_um_valor.py
+++ Regressão_um_valor/autos_regressão_um_valor.py
@@ -26,4 +26,33 @@ base = base[base.price > 10]
 i2 = base.loc[base.price > 350000]
 base = base.loc[base.price < 350000]
 
-base.loc[pd.isnull(base('vehicleType'))]
+base.loc[pd.isnull(base['vehicleType'])]
+base['vehicleType'].value_counts() #limousine
+base.loc[pd.isnull(base['gearbox'])]
+base['gearbox'].value_counts()     #manuell
+base.loc[pd.isnull(base['model'])]
+base['model'].value_counts()     #golf
+base.loc[pd.isnull(base['fuelType'])]
+base['fuelType'].value_counts()  #benzin
+base.loc[pd.isnull(base['notRepairedDamage'])]
+base['notRepairedDamage'].value_counts()  #nein
+
+valores = {'vehicleType' : 'limousine', 'gearbox' : 'manuell',
+           'model' : 'golf', 'fuelType' : 'benzin'
+           , 'notRepairedDamage' : 'nein'}
+base = base.fillna(value = valores)
+
+previsores = base.iloc[:, 1:13].values
+preco_real = base.iloc[:,0].values
+
+from sklearn.preprocessing import LabelEncoder
+labelencoder_previsores = LabelEncoder()
+
+
+previsores[:, 0] = labelencoder_previsores.fit_transform(previsores[:,0])
+previsores[:, 1] = labelencoder_previsores.fit_transform(previsores[:,1])
+previsores[:, 3] = labelencoder_previsores.fit_transform(previsores[:,3])
+previsores[:, 5] = labelencoder_previsores.fit_transform(previsores[:,5])
+previsores[:, 8] = labelencoder_previsores.fit_transform(previsores[:,8])
+previsores[:, 9] = labelencoder_previsores.fit_transform(previsores[:,9])
+previsores[:, 10] = labelencoder_previsores.fit_transform(previsores[:,10])
