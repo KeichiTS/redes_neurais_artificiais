80541f95773b10468d9ab33e36871e08b350dde4
KeichiTS <kkeichi@gmail.co>
2023-04-20 13:18:14 -0300

    autos_regressão_um_valor_Ver5_final

--- Regressão_um_valor/autos_regressão_um_valor.py
+++ Regressão_um_valor/autos_regressão_um_valor.py
@@ -5,6 +5,8 @@ Created on Mon Apr 17 19:06:45 2023
 @author: KeichiTS
 """
 import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense 
 
 base = pd.read_csv('autos.csv', encoding = 'ISO-8859-1')
 base = base.drop('dateCrawled', axis = 1)
@@ -45,14 +47,27 @@ base = base.fillna(value = valores)
 previsores = base.iloc[:, 1:13].values
 preco_real = base.iloc[:,0].values
 
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 labelencoder_previsores = LabelEncoder()
+previsores[:, 0] = labelencoder_previsores.fit_transform(previsores[:, 0])
+previsores[:, 1] = labelencoder_previsores.fit_transform(previsores[:, 1])
+previsores[:, 3] = labelencoder_previsores.fit_transform(previsores[:, 3])
+previsores[:, 5] = labelencoder_previsores.fit_transform(previsores[:, 5])
+previsores[:, 8] = labelencoder_previsores.fit_transform(previsores[:, 8])
+previsores[:, 9] = labelencoder_previsores.fit_transform(previsores[:, 9])
+previsores[:, 10] = labelencoder_previsores.fit_transform(previsores[:, 10])
 
+onehotencorder = ColumnTransformer(transformers=[("OneHot", OneHotEncoder(), [0,1,3,5,8,9,10])],remainder='passthrough')
+previsores = onehotencorder.fit_transform(previsores).toarray()
 
-previsores[:, 0] = labelencoder_previsores.fit_transform(previsores[:,0])
-previsores[:, 1] = labelencoder_previsores.fit_transform(previsores[:,1])
-previsores[:, 3] = labelencoder_previsores.fit_transform(previsores[:,3])
-previsores[:, 5] = labelencoder_previsores.fit_transform(previsores[:,5])
-previsores[:, 8] = labelencoder_previsores.fit_transform(previsores[:,8])
-previsores[:, 9] = labelencoder_previsores.fit_transform(previsores[:,9])
-previsores[:, 10] = labelencoder_previsores.fit_transform(previsores[:,10])
+regressor = Sequential()
+regressor.add(Dense(units=158, activation='relu', input_dim = 316))
+regressor.add(Dense(units = 158, activation = 'relu'))
+regressor.add(Dense(units = 1, activation = 'linear'))
+regressor.compile(loss = 'mean_absolute_error', optimizer = 'adam',
+                 metrics = ['mean_absolute_error'])
+regressor.fit(previsores,preco_real,batch_size = 300, epochs = 100)
+
+previsores = regressor.predict(previsores)
+preco_real.mean()
