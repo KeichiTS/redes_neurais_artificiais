467696b7a8ea7a06b11140c4c1e5f0cc54805020
unknown <kkeichi@gmail.co>
2023-04-20 14:06:31 -0300

    teste

--- Regressão_um_valor/autos_regressão_cruzada.py
+++ Regressão_um_valor/autos_regressão_cruzada.py
@@ -80,4 +80,4 @@ resultados = cross_val_score(estimator = regressor, X = previsores,
                              y = preco_real, cv = 10, scoring = 'neg_mean_absolute_error')
 media = resultados.mean()
 desvio = resultados.std()
-#teste
+#teste2
