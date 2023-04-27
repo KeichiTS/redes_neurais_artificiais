00a82c84c0394bece5176ba237c44ed967ff1649
KeichiTS <kkeichi@gmail.com>
2023-04-27 14:13:01 -0300

    regressao_multipla_e_saidas_ver2

--- Regressão_multipla_e_saídas/regressao_games.py
+++ Regressão_multipla_e_saídas/regressao_games.py
@@ -10,3 +10,16 @@ from keras.layers import Dense, Dropout, Activation, Input
 from keras.models import Model
 
 base = pd.read_csv('games.csv')
+base = base.drop('Other_Sales', axis = 1)
+base = base.drop('Global_Sales', axis = 1)
+base = base.drop('Developer', axis = 1)
+
+base = base.dropna(axis = 0)
+base = base.loc[base['NA_Sales'] > 1]
+base = base.loc[base['EU_Sales'] > 1]
+
+base['Name'].value_counts()
+nome_jogos = base.Name
+#plataforma_jogos = base.Platform  #apenas um teste para ver como funcionada o base
+base = base.drop('Name', axis = 1)
+
