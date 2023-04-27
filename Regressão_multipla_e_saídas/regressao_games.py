a7ce68664ad84c9639e2ba61554f091d6bf966ee
KeichiTS <kkeichi@gmail.com>
2023-04-27 14:48:49 -0300

    regressao_multipla_e_saidas_ver3_final

--- Regressão_multipla_e_saídas/regressao_games.py
+++ Regressão_multipla_e_saídas/regressao_games.py
@@ -39,3 +39,19 @@ previsores[:,8] = labelencoder.fit_transform(previsores[:,8])
 #onehotencoder = OneHotEncoder(categorical_features = [0,2,3,8]) #essa linha precisou de ajustes 
 onehotencoder = ColumnTransformer(transformers=[("OneHot", OneHotEncoder(), [0,2,3,8])],remainder='passthrough')
 previsores = onehotencoder.fit_transform(previsores).toarray()
+
+camada_entrada = Input(shape=(61,))
+camada_oculta1 = Dense(units = 32, activation = 'sigmoid')(camada_entrada)
+camada_oculta2 = Dense(units = 32, activation = 'sigmoid')(camada_oculta1)
+camada_saida1 = Dense(units = 1, activation= 'linear')(camada_oculta2)
+camada_saida2 = Dense(units = 1, activation= 'linear')(camada_oculta2)
+camada_saida3 = Dense(units = 1, activation= 'linear')(camada_oculta2)
+
+regressor = Model(inputs = camada_entrada,
+                  outputs = [camada_saida1, camada_saida2, camada_saida3])
+regressor.compile(optimizer = 'adam', loss = 'mse')
+regressor.fit(previsores, [venda_na, venda_eu, venda_jp],
+              epochs = 5000, batch_size = 100)
+
+previsao_na, previsao_eu, previsao_jp = regressor.predict(previsores)
+
