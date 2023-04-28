67d27b4b54398f49449d290d8c0582b910b3e4f5
KeichiTS <kkeichi@gmail.com>
2023-04-28 16:27:48 -0300

    gato_cachorro_ver2

--- gatos_cachorros/gato_cachorro.py
+++ gatos_cachorros/gato_cachorro.py
@@ -31,3 +31,27 @@ classificador.add(Dense(units = 1, activation = 'sigmoid'))
 
 classificador.compile(optimizer = 'adam', loss = 'binary_crossentropy',
                       metrics = ['accuracy'])
+
+gerador_treinamento = ImageDataGenerator(rescale = 1./255, 
+                                         rotation_range = 7,
+                                         horizontal_flip = True,
+                                         shear_range = 0.2,
+                                         height_shift_range = 0.07,
+                                         zoom_range = 0.2
+                                         )
+gerador_teste = ImageDataGenerator(rescale = 1./255)
+
+base_treinamento = gerador_treinamento.flow_from_directory('dataset/training_set',
+                                                           target_size = (64,64),
+                                                           batch_size = 32,
+                                                           class_mode = 'binary')
+
+base_teste = gerador_teste.flow_from_directory('dataset/test_set',
+                                               target_size = (64, 64),
+                                               batch_size = 32,
+                                               class_mode = 'binary')
+
+classificador.fit_generator(base_treinamento, steps_per_epoch = 4000/32,
+                            epochs = 5, validation_data = base_teste,
+                            validation_steps = 1000/32
+                            )
