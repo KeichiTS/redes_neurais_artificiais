8c625b54e3737f2282be30bd14de2a185fadd388
KeichiTS <kkeichi@gmail.com>
2023-05-03 21:09:01 -0300

    ajustes

--- redes_neurais_recorrentes/petr4_recorrente_um_valor.py
+++ redes_neurais_recorrentes/petr4_recorrente_um_valor.py
@@ -7,7 +7,13 @@ Created on Wed May  3 19:18:11 2023
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
+from sklearn.preprocessing import MinMaxScaler
 import numpy as pd 
 import pandas as pd
 
 base = pd.read_csv('petr4_treinamento.csv')
+base = base.dropna()
+base_treinamento = base.iloc[:,1:2].values
+
+normalizador = MinMaxScaler(feature_range = (0,1))
+base_treinamento_normalizada = normalizador.fit_transform(base_treinamento)
