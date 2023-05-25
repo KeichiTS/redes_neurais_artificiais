28440c32c633145a3eb1e6ad41120ebf16438b10
KeichiTS <kkeichi@gmail.com>
2023-05-25 15:21:59 -0300

    poluicao_rede_montada_fixes

--- redes_neurais_recorrentes/poluicao.py
+++ redes_neurais_recorrentes/poluicao.py
@@ -6,50 +6,77 @@ Created on Tue May 23 18:36:52 2023
 """
 
 from keras.models import Sequential
-from keras.layers import Dense, Dropout,LSTM
+from keras.layers import Dense, LSTM, Dropout
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 
+
 base = pd.read_csv('poluicao.csv')
+
 base = base.dropna()
-base_treinamento = base.drop(columns = 'cbwd')
-base_treinamento = base_treinamento.iloc[:,5:].values
 
-normalizador = MinMaxScaler(feature_range=(0,1))
+base = base.drop('No', axis = 1)
+base = base.drop('year', axis = 1)
+base = base.drop('month', axis = 1)
+base = base.drop('day', axis = 1)
+base = base.drop('hour', axis = 1)
+base = base.drop('cbwd', axis = 1)
+
+base_treinamento = base.iloc[:, 1:7].values
+
+poluicao = base.iloc[:, 0].values
+
+normalizador = MinMaxScaler(feature_range = (0, 1))
 base_treinamento_normalizada = normalizador.fit_transform(base_treinamento)
+poluicao = poluicao.reshape(-1, 1)
+poluicao_normalizado = normalizador.fit_transform(poluicao)
 
-normalizador_previsao = MinMaxScaler(feature_range = (0,1))
-normalizador_previsao.fit_transform(base_treinamento[:,0:1])
 
 previsores = []
-preco_real = []
-for i in range(90,1242):
-    previsores.append(base_treinamento_normalizada[i-90:i,0:6])
-    preco_real.append(base_treinamento_normalizada[i,0])
-previsores, preco_real = np.array(previsores), np.array(preco_real)
+poluicao_real = []
+for i in range(10, 41757):
+    previsores.append(base_treinamento_normalizada[i-10:i, 0:6])
+    poluicao_real.append(poluicao_normalizado[i, 0])
+previsores, poluicao_real = np.array(previsores), np.array(poluicao_real)
 
 regressor = Sequential()
 regressor.add(LSTM(units = 100, return_sequences = True, input_shape = (previsores.shape[1], 6)))
 regressor.add(Dropout(0.3))
 
 regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.3))
+regressor.add(Dropout(0.2))
 
 regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.3))
+regressor.add(Dropout(0.2))
 
 regressor.add(LSTM(units = 50))
-regressor.add(Dropout(0.3))
+regressor.add(Dropout(0.2))
 
-regressor.add(Dense(units = 1, activation = 'sigmoid'))
+regressor.add(Dense(units = 1, activation = 'linear'))
 
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_error', 
+regressor.compile(optimizer = 'rmsprop', loss = 'mean_squared_error', 
                   metrics = ['mean_absolute_error'])
+regressor.fit(previsores, poluicao_real, epochs = 100, batch_size = 64)
 
 es = EarlyStopping(monitor = 'loss', min_delta = 1e-10, patience = 10, verbose = 1)
 rlr = ReduceLROnPlateau(monitor = 'loss', factor = 0.2, patience = 5, verbose = 1)
 mcp = ModelCheckpoint(filepath = 'pesos.h5', monitor = 'loss', save_best_only = True)
-regressor.fit(previsores, preco_real, epochs = 100, batch_size = 32, callbacks = [es, rlr, mcp])
+regressor.fit(previsores, poluicao, epochs = 100, batch_size = 32, callbacks = [es, rlr, mcp])
+
+
+previsoes = regressor.predict(previsores)
+previsoes = normalizador.inverse_transform(previsoes)
+
+previsoes.mean()
+poluicao.mean()
+
+plt.plot(poluicao, color = 'red', label = 'Poluição real')
+plt.plot(previsoes, color = 'blue', label = 'Previsões')
+plt.title('Previsão poluição')
+plt.xlabel('Horas')
+plt.ylabel('Valor poluição')
+plt.legend()
+plt.show()
