999406ba71acae01030ce33c43a0badf92f3cb44
KeichiTS <kkeichi@gmail.com>
2023-09-14 17:10:49 -0300

    87. Construção do mapa auto organizável

--- Agrupamento_de_tipos_de_vinho/vinhos_mapa.py
+++ Agrupamento_de_tipos_de_vinho/vinhos_mapa.py
@@ -17,3 +17,16 @@ from sklearn.preprocessing import MinMaxScaler
 normalizador = MinMaxScaler(feature_range = (0,1))
 
 X = normalizador.fit_transform(X)
+
+som = MiniSom( x = 8, y = 8, input_len = 13, sigma = 1.0, learning_rate = 0.5, random_seed = 2 )
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+
+som._weights
+som._activation_map
+q = som.activation_response(X)
+
+from pylab import pcolor, colorbar
+pcolor(som.distance_map().T)
+#MID - mean inter neuron distance 
+colorbar()
