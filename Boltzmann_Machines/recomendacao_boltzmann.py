614079e952a610e794ac6fddb2707346137c62c5
KeichiTS <kkeichi@gmail.com>
2023-09-20 14:02:22 -0300

    98. Recomendação de filmes com RBM

--- Boltzmann_Machines/recomendacao_boltzmann.py
+++ Boltzmann_Machines/recomendacao_boltzmann.py
@@ -16,5 +16,21 @@ base = np.array([[1,1,1,0,0,0],
                  [0,0,1,1,1,1],
                  [0,0,1,1,0,1],
                  [0,0,1,1,0,1]])
+
+filmes = ['A bruxa', 'Invocação do Mal', 'O Chamado', 'Se beber não case', 'Gente Grande', 'American Pie']
+
 rbm.train(base, max_epochs = 5000)
 rbm.weights
+
+usuario1 = np.array([[1,1,0,1,0,0]])
+usuario2 = np.array([[0,0,0,1,1,0]])
+
+rbm.run_visible(usuario1)
+rbm.run_visible(usuario2)
+
+camada_escondida = np.array([[1,0]])
+recomendacao = rbm.run_hidden(camada_escondida)
+
+for i in range(len(usuario1[0])):
+    if usuario1[0,i] == 0 and recomendacao[0,i] == 1:
+        print(filmes[i])
