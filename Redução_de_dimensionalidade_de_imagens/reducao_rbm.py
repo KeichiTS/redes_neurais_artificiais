2b87ee136223545d759dc394a7178d403f30c8e8
KeichiTS <kkeichi@gmail.com>
2023-10-10 16:00:12 -0300

    101. RBM x sem RBM

--- Redução_de_dimensionalidade_de_imagens/reducao_rbm.py
+++ Redução_de_dimensionalidade_de_imagens/reducao_rbm.py
@@ -25,3 +25,25 @@ previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = tra
                                                                                               classe,
                                                                                               test_size = 0.2,
                                                                                               random_state = 0)
+rbm = BernoulliRBM(random_state = 0)
+rbm.n_iter = 25
+rbm.n_components = 50 
+naive_rbm = GaussianNB()
+classificador_rbm = Pipeline(steps = [('rbm', rbm), ('naive', naive_rbm)])
+classificador_rbm.fit(previsores_treinamento, classe_treinamento)
+
+plt.figure(figsize = (20,20))
+for i, comp in enumerate(rbm.components_):
+    plt.subplot(10, 10, i + 1)
+    plt.imshow(comp.reshape(8,8), cmap = plt.cm.gray_r)
+    plt.xticks(())
+    plt.yticks(())
+plt.show()
+
+previsores_rbm = classificador_rbm.predict(previsores_teste)
+precisao_rbm = metrics.accuracy_score(previsores_rbm, classe_teste)
+
+naive_simples = GaussianNB()
+naive_simples.fit(previsores_treinamento, classe_treinamento)
+previsoes_naive = naive_simples.predict(previsores_teste)
+precisao_naive = metrics.accuracy_score(previsoes_naive, classe_teste)
