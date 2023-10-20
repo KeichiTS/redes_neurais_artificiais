47412770523109c65685424aeb89adbb4af4c8ad
KeichiTS <kkeichi@gmail.com>
2023-10-20 17:17:48 -0300

    113. Convolutional autoencoders II

--- Redução_de_dimensionalidade_de_imagem/convolutional_autoencoder.py
+++ Redução_de_dimensionalidade_de_imagem/convolutional_autoencoder.py
@@ -34,5 +34,52 @@ autoencoder.add(Conv2D(filters = 8, kernel_size = (3,3), activation = 'relu', pa
 
 autoencoder.add(Flatten())
 
-autoencoder.add(Reshape(4, 4, 8))
+autoencoder.add(Reshape((4, 4, 8)))
 
+
+#Decoder
+autoencoder.add(Conv2D(filters = 8, kernel_size = (3,3), activation = 'relu', padding = 'same'))
+autoencoder.add(UpSampling2D(size = ( 2 , 2 )))
+autoencoder.add(Conv2D(filters = 8, kernel_size = (3,3), activation = 'relu', padding = 'same'))
+autoencoder.add(UpSampling2D(size = ( 2 , 2 )))
+autoencoder.add(Conv2D(filters = 16, kernel_size = (3,3), activation = 'relu'))
+autoencoder.add(UpSampling2D(size = ( 2 , 2 )))
+autoencoder.add(Conv2D(filters = 1, kernel_size = (3,3), activation = 'sigmoid', padding = 'same'))
+autoencoder.summary()
+
+autoencoder.compile(optimizer = 'adam', loss = 'binary_crossentropy', 
+                    metrics = ['accuracy']) 
+autoencoder.fit(previsores_treinamento, previsores_treinamento,
+                epochs = 10, batch_size = 256,
+                validation_data = (previsores_teste, previsores_teste)) 
+
+encoder = Model(inputs = autoencoder.input, outputs = autoencoder.get_layer('flatten_7').output)
+encoder.summary()
+
+imagens_codificadas = encoder.predict(previsores_teste)
+imagens_decodificadas = autoencoder.predict(previsores_teste)
+
+numero_imagens = 10
+imagens_teste = np.random.randint(previsores_teste.shape[0], size = numero_imagens)
+plt.figure(figsize = (18,18))
+for i, indice_imagem in enumerate(imagens_teste):
+    #print(i)
+    #print(indice_imagem)
+    
+    # imagem original
+    eixo = plt.subplot(10, 10, i + 1)
+    plt.imshow(previsores_teste[indice_imagem].reshape(28,28))
+    plt.xticks(())
+    plt.yticks(())
+    
+    # imagem codificada
+    eixo = plt.subplot(10, 10, i + 1 + numero_imagens)
+    plt.imshow(imagens_codificadas[indice_imagem].reshape(16,8))
+    plt.xticks(())
+    plt.yticks(())
+    
+    # imagem reconstruida
+    eixo = plt.subplot(10, 10, i + 1 + numero_imagens * 2 )
+    plt.imshow(imagens_decodificadas[indice_imagem].reshape(28,28))
+    plt.xticks(())
+    plt.yticks(())
